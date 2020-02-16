565578d73e9e641d897df2ec4eddd04ca8b39801
Silence <sunwei06294@hellobike.com>
2020-02-16 18:25:35 +0800

    feat(导航模块完成): 体系切换卡顿，列表加载卡顿待处理，搜索模块和项目模块待开发

---
+++ lib/navigation/ArticleListPage.dart
@@ -0,0 +1,3 @@
+/// @date:2020-02-16
+/// @author:Silence
+/// @describe:
--- lib/web/WebViewPage.dart
+++ lib/web/WebViewPage.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:silence_wan_android/net/ApiUrl.dart';
 import 'package:silence_wan_android/net/HttpUtils.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 
 import '../common/DataUtils.dart';
 
@@ -29,37 +29,74 @@ class WebViewPage extends StatefulWidget {
 
 class _WebViewPage extends State<WebViewPage> {
   bool collect;
+  FlutterWebviewPlugin flutterWebViewPlugin = FlutterWebviewPlugin();
+  double lineProgress = 0.0;
 
   _WebViewPage({this.collect});
 
   @override
+  void initState() {
+    super.initState();
+    flutterWebViewPlugin.onProgressChanged.listen((progress) {
+      print(progress);
+      setState(() {
+        lineProgress = progress;
+      });
+    });
+    flutterWebViewPlugin.onDestroy.listen((_) {
+      Navigator.of(context).pop();
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    flutterWebViewPlugin.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return WebviewScaffold(
       appBar: AppBar(
-        title: Text(DataUtils.replaceAll(widget.webTitle)),
-        centerTitle: true,
-        actions: <Widget>[
-          Visibility(
-            visible: widget.isCanCollect,
-            child: IconButton(
-              icon: Icon(
-                collect ? Icons.favorite : Icons.favorite_border,
-                color: collect ? Colors.red : null,
+          title: Text(DataUtils.replaceAll(widget.webTitle)),
+          centerTitle: true,
+          actions: <Widget>[
+            Visibility(
+              visible: widget.isCanCollect,
+              child: IconButton(
+                icon: Icon(
+                  collect ? Icons.favorite : Icons.favorite_border,
+                  color: collect ? Colors.red : null,
+                ),
+                onPressed: () {
+                  if (collect) {
+                    _unCollect();
+                  } else {
+                    _collect();
+                  }
+                },
               ),
-              onPressed: () {
-                if (collect) {
-                  _unCollect();
-                } else {
-                  _collect();
-                }
-              },
             ),
-          ),
-        ],
+          ],
+          bottom: PreferredSize(
+            child: _progressBar(lineProgress, context),
+            preferredSize: Size.fromHeight(1.0),
+          )),
+      url: widget.webUrl,
+      withZoom: false,
+      withLocalStorage: true,
+      withJavascript: true,
+    );
+  }
+
+  _progressBar(double progress, BuildContext context) {
+    return Container(
+      child: LinearProgressIndicator(
+        backgroundColor: Colors.white70.withOpacity(0),
+        value: progress == 1.0 ? 0 : progress,
+        valueColor: new AlwaysStoppedAnimation<Color>(Colors.tealAccent),
       ),
-      body: WebView(
-          initialUrl: widget.webUrl,
-          javascriptMode: JavascriptMode.unrestricted),
+      height: 2.0,
     );
   }
 
